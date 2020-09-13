5c6fe925e99ae24d70c09f6566d25ebe86506646
j- <j@skeoh.com>
2020-09-13 15:27:02 +1000

    Updates

---
+++ precache-manifest.257dd837f14a7f263e5af227012d4bce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25cfdb17a737a851a9505f5130ee2f66",
+    "url": "/file-to-data-url/index.html"
+  },
+  {
+    "revision": "156a22283ef0701166b7",
+    "url": "/file-to-data-url/static/css/2.5613e36a.chunk.css"
+  },
+  {
+    "revision": "156a22283ef0701166b7",
+    "url": "/file-to-data-url/static/js/2.6dbe6593.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/file-to-data-url/static/js/2.6dbe6593.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65a64e7d5c1739676012",
+    "url": "/file-to-data-url/static/js/main.5424d981.chunk.js"
+  },
+  {
+    "revision": "266dd33448f8f9f020a0",
+    "url": "/file-to-data-url/static/js/runtime-main.8e0f056e.js"
+  }
+]);
