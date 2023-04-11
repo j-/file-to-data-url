7999c63541b8f20c496487e11a271f189e5b12c9
j- <j@skeoh.com>
2023-04-12 09:46:58 +1000

    Animate children in and out

--- src/FilesList.tsx
+++ src/FilesList.tsx
@@ -1,12 +1,19 @@
 import * as React from 'react';
 import { useSelector } from 'react-redux';
+import autoAnimate from '@formkit/auto-animate';
 import { getFiles } from './store';
 import FilesListItem from './FilesListItem';
 
 const FilesList: React.FC = () => {
+  const parentRef = React.useRef(null);
   const files = useSelector(getFiles);
+  React.useEffect(() => {
+    if (parentRef.current) {
+      autoAnimate(parentRef.current);
+    }
+  }, [parentRef]);
   return (
-    <ol className="list-unstyled">
+    <ol ref={parentRef} className="list-unstyled">
       {files.map((file) => (
         <li key={file.blob}>
           <FilesListItem blob={file.blob} />
